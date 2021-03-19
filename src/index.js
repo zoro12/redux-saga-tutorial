321ef46f4085c22643051d7151e5f8ba4c3de294
samir taha <alrazy1@hotmail.com>
2021-03-19 21:16:32 +0200

    created the redux store users and index file and provider

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import axios from "axios";
+import reducers from "./reducers";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://rem-rest-api.herokuapp.com/api";
 
+const store = createStore(reducers);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
